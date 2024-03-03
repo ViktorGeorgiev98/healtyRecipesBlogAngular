38f305cc23a8707f491fbc6f67ec9311b8555694
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-03 18:22:01 +0200

    created footer compoenent and styling for it

--- client/healthyRecipesBlog/src/app/app.component.ts
+++ client/healthyRecipesBlog/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../components/footerComponent/footer/footer.component';
+// import { AppModule } from './app.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ client/healthyRecipesBlog/src/app/app.module.ts
@@ -0,0 +1,15 @@
+// import { NgModule } from "@angular/core";
+// import { AppComponent } from "./app.component";
+// import { FooterComponent } from "../components/footerComponent/footer/footer.component";
+
+// @NgModule({
+//     declarations: [
+//         FooterComponent,
+//     ],
+//     imports: [
+//       FooterComponent,
+//     ],
+//     providers: [],
+//     bootstrap: [AppComponent],
+//   })
+//   export class AppModule {}
