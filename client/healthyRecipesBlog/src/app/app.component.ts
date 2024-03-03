f25f8cf9e4a5a90c511ef796d1805449bb190c47
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-03 20:03:18 +0200

    created nav bar component and css, later need to add routing functionality

--- client/healthyRecipesBlog/src/app/app.component.ts
+++ client/healthyRecipesBlog/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../components/footerComponent/footer/footer.component';
+import { NavBarComponent } from '../components/navBar/nav-bar/nav-bar.component';
 // import { AppModule } from './app.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, NavBarComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
