40eba65a96279957348ed4450cea734047ade1b3
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-04 18:28:30 +0200

    added styles for register page and created submit request for register with dummy data

---
+++ client/healthyRecipesBlog/src/components/RegisterPage/register-page/register-page.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-page',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.css'
+})
+export class RegisterPageComponent {
+  submitForm(event: Event) {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const rePassword = formData.get('rePassword') as string;
+    const username = formData.get('username') as string;
+    console.log({ email, password, rePassword, username });
+    if (!email || !password || !username || !rePassword) {
+      return alert("All fields are required!"); 
+  }
+}
+}
+
+
