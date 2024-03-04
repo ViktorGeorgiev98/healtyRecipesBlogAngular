08df2701b2e991905e9a5d41ffdc3effa0d95b64
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-04 16:37:18 +0200

    created interface for user and recipe

---
+++ client/healthyRecipesBlog/src/Types/Recipe.ts
@@ -0,0 +1,8 @@
+export interface Recipe {
+    recipeName: string;
+    author: string;
+    imageUrl: string;
+    difficultyLevel: string;
+    ingredients: string;
+    instructions: string;
+}
---
+++ client/healthyRecipesBlog/src/components/recipeListComponent/recipe-list-component/recipe-list-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-list-component.component.html',
+  styleUrl: './recipe-list-component.component.css'
+})
+export class RecipeListComponentComponent {
+
+}
