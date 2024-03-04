9c5b3b68a1574cc3a0be5ae773ce5dab06aa449d
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-04 17:06:12 +0200

    created component for recipes list, now it has dummy data, later need to add crud operations and css

--- client/healthyRecipesBlog/src/app/app.component.ts
+++ client/healthyRecipesBlog/src/app/app.component.ts
@@ -3,12 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../components/footerComponent/footer/footer.component';
 import { NavBarComponent } from '../components/navBar/nav-bar/nav-bar.component';
 import { HomePageComponent } from '../components/homePage/home-page/home-page.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 // import { AppModule } from './app.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavBarComponent, HomePageComponent,],
+  imports: [RouterOutlet, FooterComponent, NavBarComponent, HomePageComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- client/healthyRecipesBlog/src/components/recipeListComponent/recipe-list-component/recipe-list-component.component.ts
+++ client/healthyRecipesBlog/src/components/recipeListComponent/recipe-list-component/recipe-list-component.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../../../Types/Recipe';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-recipe-list-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recipe-list-component.component.html',
   styleUrl: './recipe-list-component.component.css'
 })
 export class RecipeListComponentComponent {
-
+  recipes: Recipe[] = [{_id: "dsadasfafa", recipeName: "random", author: "random", shortDescription: "Top meal!", imageUrl: "https://images.immediate.co.uk/production/volatile/sites/2/2020/02/OLI-0320_Healthy-CuminCrustedChickenBreastWithKaleSaladHumous_02786_preview-eba91c9.jpg?quality=90&resize=556,505", difficultyLevel: "randomw", ingredients: "neshto, neshto, neshto", instructions: "Mix everything together"},
+  {_id: "dsadasfafa", recipeName: "random", author: "random", shortDescription: "Top meal!",  imageUrl: "https://images.immediate.co.uk/production/volatile/sites/2/2020/02/OLI-0320_Healthy-CuminCrustedChickenBreastWithKaleSaladHumous_02786_preview-eba91c9.jpg?quality=90&resize=556,505", difficultyLevel: "randomw", ingredients: "neshto, neshto, neshto", instructions: "Mix everything together"},
+  {_id: "dsadasfafa", recipeName: "random", author: "random", shortDescription: "Top meal!", imageUrl: "https://images.immediate.co.uk/production/volatile/sites/2/2020/02/OLI-0320_Healthy-CuminCrustedChickenBreastWithKaleSaladHumous_02786_preview-eba91c9.jpg?quality=90&resize=556,505", difficultyLevel: "randomw", ingredients: "neshto, neshto, neshto", instructions: "Mix everything together"}];
 }
