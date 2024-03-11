bb2dc13e821b8cde1835686cb65b781e93781908
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-11 21:46:08 +0200

    created app module, put everything inside and configured first part of routing module

--- client/healthyRecipesBlog/src/app/app.component.ts
+++ client/healthyRecipesBlog/src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { CreateRecipeComponent } from '../components/Recipes/CreateRecipe/create-recipe/create-recipe.component';
-import { CoreModule } from '../components/core/core/core.module';
-import { UserModule } from '../components/User/user/user.module';
-import { SharedModule } from '../components/shared/shared/shared.module';
+// import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule, UserModule, CoreModule, UserModule, CreateRecipeComponent, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- client/healthyRecipesBlog/src/components/shared/shared/shared.module.ts
+++ client/healthyRecipesBlog/src/components/shared/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorPageComponent } from '../ErrorPage/error-page/error-page.component';
+import { AppRoutingModule } from '../../../routing/app-routing/app-routing.module';
 
 
 
 @NgModule({
   declarations: [ErrorPageComponent],
   imports: [
-    CommonModule
+    CommonModule, AppRoutingModule
   ],
   exports: [ErrorPageComponent]
 })
