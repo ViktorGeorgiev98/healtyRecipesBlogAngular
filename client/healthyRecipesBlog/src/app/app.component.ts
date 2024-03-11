51a0cf7a095b2fab9dc73db39f7e08c8f95ca761
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-11 20:07:01 +0200

    created shared module and error page component

--- client/healthyRecipesBlog/src/app/app.component.ts
+++ client/healthyRecipesBlog/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CreateRecipeComponent } from '../components/Recipes/CreateRecipe/create-recipe/create-recipe.component';
 import { CoreModule } from '../components/core/core/core.module';
 import { UserModule } from '../components/User/user/user.module';
+import { SharedModule } from '../components/shared/shared/shared.module';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, UserModule, CoreModule, UserModule, CreateRecipeComponent],
+  imports: [RouterOutlet, CommonModule, UserModule, CoreModule, UserModule, CreateRecipeComponent, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
