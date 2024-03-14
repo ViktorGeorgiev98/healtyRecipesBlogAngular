2e91cb2aa999a840b0a57736c41ad8b38f5338cc
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-14 19:47:09 +0200

    create recipe post request start

--- client/healthyRecipesBlog/src/components/Recipes/CreateRecipe/create-recipe/create-recipe.component.ts
+++ client/healthyRecipesBlog/src/components/Recipes/CreateRecipe/create-recipe/create-recipe.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../../../API/api.service';
+import { Router } from '@angular/router';
+import { UserService } from '../../../../services/userService.service';
 
 
 @Component({
@@ -13,20 +16,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './create-recipe.component.css'
 })
 export class CreateRecipeComponent {
-  submitForm(event: Event, form: NgForm) {
-    event.preventDefault();
-    const formData = new FormData(event.target as HTMLFormElement);
-    const recipeName = formData.get('recipeName') as string;
-    const author = formData.get('author') as string;
-    const imageUrl = formData.get('imageUrl') as string;
-    const difficultyLevel = formData.get('difficultyLevel') as string;
-    const shortDescription = formData.get('shortDescription') as string;
-    const ingredients = formData.get('ingredients') as string;
-    const instructions = formData.get('instructions') as string;
-    console.log({recipeName, author, imageUrl, difficultyLevel, shortDescription, ingredients, instructions})
-    if (!recipeName || !author || !imageUrl || !difficultyLevel || !shortDescription || !ingredients || !ingredients || !instructions) {
-      return alert("All fields are mandatory! Please fill all fields.")
-    }
-    console.log("Submit create recipe.")
+  @ViewChild('createRecipeForm') form: NgForm | undefined;
+  constructor(private apiService: ApiService, private router: Router) {}
+
+
+  submitCreateRecipeForm() {
+  if (!this.form) {
+    return;
   }
+  const form = this.form;
+
+  const { recipeName, author, imageUrl, difficultyLevel, shortDescription, ingredients, instructions} = form.value;
+  console.log({ recipeName, author, imageUrl, difficultyLevel, shortDescription, ingredients})
+  if (!recipeName || !author || !imageUrl || !difficultyLevel || !shortDescription || !ingredients || !instructions) {
+    return alert("All fields are mandatory!");
+  };
+
+
+
+
+
+  }
+
 }
