aa1b011816451d39e4d5cb645511a3f35caca08e
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-14 17:27:30 +0200

    changed login form html and started writing login request

--- client/healthyRecipesBlog/src/components/User/LoginPage/login-page/login-page.component.ts
+++ client/healthyRecipesBlog/src/components/User/LoginPage/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ApiService } from '../../../../API/api.service';
+import { Router } from '@angular/router';
 // Remove import statement for NgForm
 
 @Component({
@@ -8,9 +10,26 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login-page.component.css'] // Fix styleUrl to styleUrls
 })
 export class LoginPageComponent {
-  @ViewChild('myForm', { static: true }) myForm: NgForm | undefined;
-  submitForm(event: Event) {
-    console.log("login");
-    console.log("Form value:", this.myForm?.value);
+  @ViewChild('loginForm') form: NgForm | undefined;
+  constructor(private apiService: ApiService, private router: Router) {}
+
+
+  submitLoginForm() {
+    if (!this.form) {
+      return;
+    }
+
+    const form = this.form;
+    const { email, password } = form.value;
+    const payload = {
+      email: email,
+      password: password,
+    }
+
+    if (!email || !password) {
+      return alert("All fields are required!");
+    }
+
+
   }
 }
