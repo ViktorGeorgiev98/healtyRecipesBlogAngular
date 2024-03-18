bab9616ccb49e40ca55eca13fa9e63a5ee5266b0
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-18 18:42:36 +0200

    styles for the all recipes page, added spinner

--- client/healthyRecipesBlog/src/components/core/recipes/all-recipes/all-recipes.component.ts
+++ client/healthyRecipesBlog/src/components/core/recipes/all-recipes/all-recipes.component.ts
@@ -1,19 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../../API/api.service';
 import { Recipe } from '../../../../Types/Recipe';
 
 @Component({
   selector: 'app-all-recipes',
-  standalone: true,
-  imports: [],
   templateUrl: './all-recipes.component.html',
   styleUrl: './all-recipes.component.css'
 })
-export class AllRecipesComponent {
+export class AllRecipesComponent implements OnInit {
   constructor(private apiService:ApiService) {}
 
-  recipes: Recipe[] = [];
+  recipesList: Recipe[] = [];
   isLoading: boolean = true;
+  currentPage: number = 1;
+  pageSize: number = 6; // Number of items per page
+  totalItems: number = 0;
 
-  
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+      this.apiService.getAllRecipes().subscribe({
+        next: (recipes: any) => {
+          console.log(recipes);
+          this.recipesList = recipes;
+        },
+        error: (error: any) => {
+          console.error(error.message);
+          this.recipesList = [];
+        }
+      });
+    }, 2500);
+  }
+
 }
