3e22c209e239bbbb52b2ac8b3529a92917e23c36
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-03-30 07:43:33 +0200

    more animations

--- client/healthyRecipesBlog/src/components/core/navBar/nav-bar/nav-bar.component.ts
+++ client/healthyRecipesBlog/src/components/core/navBar/nav-bar/nav-bar.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../../../services/userService.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrl: './nav-bar.component.css',
+  animations: [
+    trigger('myAnimationTrigger', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate('1000ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('1000ms ease-in', style({ transform: 'translateY(100%)', opacity: 0 }))
+      ]),
+    ]),
+  ],
 })
 export class NavBarComponent {
   constructor(private router: Router, private userService: UserService) {}
